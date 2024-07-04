f215112333e8d754c6e2d04252102270c78bcd02
Pratham Gupta <guptapratham443@gmail.com>
2024-07-05 03:54:48 +0530

    Chnaged db to render psql

--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('bitespeed_db', 'admin', 'pass', {
-  host: 'localhost',
-  dialect: 'postgres'
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: true, // Enable SSL for secure connection (if applicable)
+  }
 });
 
 module.exports = sequelize;
