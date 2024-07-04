69d349b436821d8165e4549097864de9fdac96e9
Pratham Gupta <guptapratham443@gmail.com>
2024-07-05 03:58:46 +0530

    Removed ssl true

--- config/database.js
+++ config/database.js
@@ -1,10 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: true, // Enable SSL for secure connection (if applicable)
-  }
+  dialect: 'postgres'
 });
 
 module.exports = sequelize;
